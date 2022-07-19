b07df10b0c93b4474be3ee33fa8e0f4caadf6e20
Hansol <whlthd1207@naver.com>
2022-07-19 13:59:50 +0900

    20220719_Clock추가

--- js/greetings.js
+++ js/greetings.js
@@ -3,9 +3,10 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 const loginForm = document.querySelector("#login-form");
 const greeting = document.querySelector("#greeting");
-
 const link = document.querySelector("a");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginBtnClick(){
    //console.dir(loginInput);
@@ -22,12 +23,11 @@ function onLoginBtnClick(){
 
 function onLoginSubmit(tomato){
     tomato.preventDefault();    
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const usernameThatTheUserWrote = loginInput.value;
     //greeting.innerText = "Hello" + username; 아래와 같음
-    localStorage.setItem("username",username);  //로컬스토리지에 저장
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY,usernameThatTheUserWrote);  //로컬스토리지에 저장
+    paintGreetings(usernameThatTheUserWrote);
 }
 
 function handleLinkClick(event){
@@ -36,7 +36,25 @@ function handleLinkClick(event){
     alert("clicked!");
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-
 loginButton.addEventListener("click", onLoginBtnClick);
 link.addEventListener("click", handleLinkClick);
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+//console.log(savedUsername);
+
+if(savedUsername == null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+}else{
+    //show the greeting
+    paintGreetings(savedUsername);
+    }
